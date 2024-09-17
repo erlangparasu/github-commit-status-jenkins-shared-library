39f46d171b1d3e5f04b5148574f7b371e3c4074f
Erlang Parasu <erlangparasu@gmail.com>
2024-09-18 07:34:30 +0800

    feat: commitStatus curl

--- vars/commitStatus.groovy
+++ vars/commitStatus.groovy
@@ -1,4 +1,4 @@
-def call(String name = "Unknown") {
+def call(String name = "Unknown", Map config) {
     script {
         sh """#!/bin/bash
             set -e
@@ -7,6 +7,14 @@ def call(String name = "Unknown") {
             pwd
             ls -laF .
             echo Hi ${name}
+            #
+            curl -L \
+                -X POST \
+                -H "Accept: application/vnd.github+json" \
+                -H "Authorization: Bearer ${config.token}" \
+                -H "X-GitHub-Api-Version: 2022-11-28" \
+                https://api.github.com/repos/${config.owner}/${config.repo}/statuses/${config.sha} \
+                -d '{"state":"${config.state}","target_url":"${config.targetUrl}","description":"${config.desc}","context":"${config.context}"}'
         """
     }
 }
